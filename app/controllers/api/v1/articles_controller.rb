fa66563f153b38f2092ff7d9336d496f2875068e
maciekbaranowskiqq <maciek.baranowski.qq@gmail.com>
2017-12-14 17:34:30 +0100

    initial commit

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,30 @@
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+      def index
+        articles = Article.order('created_at DESC');
+        render json: {status: 'success', message: 'loaded articles', data:articles}, status: :ok
+      end
+      def show
+        articles = Article.find(params[:id]);
+        render json: {status: 'success', message: 'loaded article', data:articles}, status: :ok
+      end
+
+      def create
+        article = Article.new(article_params);
+
+        if article.save
+          render json: {status: 'success', message: 'loaded articles', data:article}, status: :ok
+        else
+          render json: {status: 'error', message: 'article not saved', data:article.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+      def article_params
+        params.permit(:title, :body)
+      end
+
+    end
+  end
+end
---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Myapp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    config.session_store :cookie_store
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    config.middleware.use Rack::MethodOverride
+    
+  end
+end
