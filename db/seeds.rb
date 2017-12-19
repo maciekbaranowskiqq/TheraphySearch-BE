64d0ceb19f3fc4a5bfc571df51aad070b63e6d76
maciekbaranowskiqq <maciek.baranowski.qq@gmail.com>
2017-12-19 15:45:56 +0100

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
+# 5.times do
+#   Article.create({
+#     title: Faker::Book.title,
+#     body: Faker::Lorem.sentence
+#     })
+# end
+
 5.times do
-  Article.create({
-    title: Faker::Book.title,
-    body: Faker::Lorem.sentence
+  Counsellor.create({
+    name: Faker::Name.name
     })
 end
