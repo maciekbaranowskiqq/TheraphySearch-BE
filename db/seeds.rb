f6dda242bd2c0b60d92ea81971e073a5098b326e
maciekbaranowskiqq <maciek.baranowski.qq@gmail.com>
2017-12-19 17:31:44 +0100

    small fixes in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,45 +7,45 @@ end
 
 3.times do
   Counsellor.create({
-    name: Faker::Name.title,
+    name: Faker::Name.name,
     profile_picture: "https://images.unsplash.com/photo-1478480154178-88540b2be3b0?dpr=1&auto=format&fit=crop&w=376&h=213&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
     city: Faker::Address.city,
     address: Faker::Address.street_name,
     latitude: Faker::Address.latitude,
-    latitude: Faker::Address.longitude,
+    longitude: Faker::Address.longitude,
     paradigm: "CBT",
     specialization: "short term CBT theraphy",
     cost: "50"
     })
 end
 
-# 3.times do
-#   Counsellor.create({
-#     name: Faker::Name.title,
-#     profile_picture: "https://images.unsplash.com/photo-1478480154178-88540b2be3b0?dpr=1&auto=format&fit=crop&w=376&h=213&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
-#     city: Faker::Address.city,
-#     address: Faker::Address.street_name,
-#     latitude: Faker::Address.latitude,
-#     latitude: Faker::Address.longitude,
-#     paradigm: "psychodynamic",
-#     specialization: "long term, based on relationships",
-#     cost: "100"
-#     })
-# end
-#
-# 3.times do
-#   Counsellor.create({
-#     name: Faker::Name.title,
-#     profile_picture: "https://images.unsplash.com/photo-1478480154178-88540b2be3b0?dpr=1&auto=format&fit=crop&w=376&h=213&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
-#     city: Faker::Address.city,
-#     address: Faker::Address.street_name,
-#     latitude: Faker::Address.latitude,
-#     latitude: Faker::Address.longitude,
-#     paradigm: "psychologist",
-#     specialization: "consulations, tests, diagnosis",
-#     cost: "20"
-#     })
-# end
+3.times do
+  Counsellor.create({
+    name: Faker::Name.name,
+    profile_picture: "https://images.unsplash.com/photo-1478480154178-88540b2be3b0?dpr=1&auto=format&fit=crop&w=376&h=213&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
+    city: Faker::Address.city,
+    address: Faker::Address.street_name,
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+    paradigm: "psychodynamic",
+    specialization: "long term, based on relationships",
+    cost: "100"
+    })
+end
+
+3.times do
+  Counsellor.create({
+    name: Faker::Name.name,
+    profile_picture: "https://images.unsplash.com/photo-1478480154178-88540b2be3b0?dpr=1&auto=format&fit=crop&w=376&h=213&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
+    city: Faker::Address.city,
+    address: Faker::Address.street_name,
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+    paradigm: "psychologist",
+    specialization: "consulations, tests, diagnosis",
+    cost: "20"
+    })
+end
 
 5.times do
   Post.create({
@@ -53,6 +53,6 @@ end
     image: "https://images.unsplash.com/photo-1478480154178-88540b2be3b0?dpr=1&auto=format&fit=crop&w=376&h=213&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
     owner_picture: "http://www.deluxxe.co.uk/assets/Ruth_new_colour_closeup.jpg",
     owner_name: Faker::Name.name,
-    count_likes: Faker::Number.number(10)
+    count_likes: Faker::Number.number(1)
     })
 end
