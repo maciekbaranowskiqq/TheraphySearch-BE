0588728fd91e3434462d945ec826b73f0582b90e
maciekbaranowskiqq <maciek.baranowski.qq@gmail.com>
2017-12-19 16:14:17 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# 5.times do
-#   Article.create({
-#     title: Faker::Book.title,
-#     body: Faker::Lorem.sentence
-#     })
-# end
+5.times do
+  Article.create({
+    title: Faker::Book.title,
+    body: Faker::Lorem.sentence
+    })
+end
 
 5.times do
   Counsellor.create({
